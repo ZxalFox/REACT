4a644aa4bd6f75a5cdb8bc6bd7d6aca63c1f5e35
Ayron Sanfra <ayron.marinho@estudante.ufla.br>
2023-07-19 13:21:18 -0300

    #

--- ex06/src/App.jsx
+++ ex06/src/App.jsx
@@ -1,14 +1,20 @@
 import React from "react";
-import Header from "./header";
-import Footer from "./Footer";
-import Form from "./Form/Form";
+import Header from "./Header";
+import Home from "./Home";
+import Produtos from "./Produtos";
 
 const App = () => {
+  let Pagina = Home;
+  const {pathname} = window.location;
+  if(pathname == "/produtos") {
+    Pagina = Produtos
+  }else{
+    Pagina = Home
+  }
   return(
     <div>
       <Header />
-      <Form />
-      <Footer />
+      <Pagina />
     </div>
   );
 }
--- ex06/src/Header.jsx
+++ ex06/src/Header.jsx
@@ -2,7 +2,12 @@ import React from "react";
 
 const Header = () =>{
     return(
-        <header>Header</header>
+        <header>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/produtos">Produtos</a></li>
+            </ul>
+        </header>
     );
 }
 
