01eed2e644b9044b6fc477032c79ffd97bbaa724
Ayron Sanfra <ayron.marinho@estudante.ufla.br>
2023-07-27 12:41:01 -0300

    atl login page

--- projeto-dogs/src/App.jsx
+++ projeto-dogs/src/App.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
-import './App.css'
-import Api from './api/Api'
+import React from 'react';
+import './App.css';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Home from './Components/Home'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './Components/Login/Login';
 
 
-const App = () => {
+function App (){
    return (
     <div>
-      <Api />
+      <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login/*" element={< Login /> } />
+      </Routes>
+      <Footer />
+      </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
